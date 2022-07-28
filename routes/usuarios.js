3b816d976ddcc603665a49688f9cefcc9532cf70
Leonardo Oliveira <leonardoancrin@gmail.com>
2022-07-28 16:27:19 -0300

    removed redirect from login route

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -82,7 +82,6 @@ router.post('/login', (req,res,next) => {
             
             bcrpyt.compare(req.body.senha, results[0].senha, (err, results) => {
                 if (results) {
-                    res.redirect('https://iotruck2.herokuapp.com/')
                     return res.status(201).send({ 
                         mensagem: 'Usuário autenticado com sucesso',
                         email: req.body.email
